6293d4b7d77cbe74564d7d8577dccfe442c84e98
tkolleh <tinatuh@gmail.com>
2019-03-15 14:56:04 -0400

    Prefer copy over add

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
 FROM postgres:10.7-alpine
 
 LABEL maintainer="tkolleh@me.com"
-
+#
 # Gather setup scripts from MIT repo
-
-ADD https://raw.githubusercontent.com/MIT-LCP/mimic-code/master/buildmimic/postgres/postgres_create_tables.sql /docker-entrypoint-initdb.d/psql/
-
-ADD https://raw.githubusercontent.com/MIT-LCP/mimic-code/master/buildmimic/postgres/postgres_add_comments.sql /docker-entrypoint-initdb.d/psql/
-
-ADD https://raw.githubusercontent.com/MIT-LCP/mimic-code/master/buildmimic/postgres/postgres_add_indexes.sql /docker-entrypoint-initdb.d/psql/
-
-ADD https://raw.githubusercontent.com/MIT-LCP/mimic-code/master/buildmimic/postgres/postgres_add_constraints.sql /docker-entrypoint-initdb.d/psql/
+# Reference: https://github.com/MIT-LCP/mimic-code
+#
+COPY postgres_create_tables.sql /docker-entrypoint-initdb.d/psql/
+COPY postgres_add_comments.sql /docker-entrypoint-initdb.d/psql/
+COPY postgres_add_indexes.sql /docker-entrypoint-initdb.d/psql/
+COPY postgres_add_constraints.sql /docker-entrypoint-initdb.d/psql/
 
 RUN chmod -R a+r /docker-entrypoint-initdb.d/psql/
 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo "Initializing MIMIC III database..."
 
@@ -19,19 +20,37 @@ EOSQL
 
 export PGOPTIONS="--search_path=MIMIC --client_min_messages=warning"
 
-echo "Running postgres_create_tables.sql"
-psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_create_tables.sql
+if [ "$CONTAINER_HOSTNAME" = "" ]
+then
+  echo "Running postgres_create_tables.sql"
+  psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_create_tables.sql
 
-echo "Running postgres_add_comments.sql"
-psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_comments.sql
+  echo "Running postgres_add_comments.sql"
+  psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_comments.sql
 
-echo "Running postgres_add_indexes.sql"
-psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_indexes.sql
+  echo "Running postgres_add_indexes.sql"
+  psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_indexes.sql
 
-echo "Running postgres_add_constraints.sql"
-psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_constraints.sql
+  echo "Running postgres_add_constraints.sql"
+  psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_constraints.sql
 
-echo "Granting all privileges on MIMIC database tables to MIMIC user"
-psql --username "$POSTGRES_USER" --dbname mimic -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA MIMIC TO MIMIC;"
+  echo "Granting all privileges on MIMIC database tables to MIMIC user"
+  psql --username "$POSTGRES_USER" --dbname mimic -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA MIMIC TO MIMIC;"
+else
+  echo "Running postgres_create_tables.sql"
+  psql --host "$CONTAINER_HOSTNAME" --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_create_tables.sql
 
-echo "Initialization complete."
+  echo "Running postgres_add_comments.sql"
+  psql --host "$CONTAINER_HOSTNAME" --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_comments.sql
+
+  echo "Running postgres_add_indexes.sql"
+  psql --host "$CONTAINER_HOSTNAME" --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_indexes.sql
+
+  echo "Running postgres_add_constraints.sql"
+  psql --host "$CONTAINER_HOSTNAME" --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_constraints.sql
+
+  echo "Granting all privileges on MIMIC database tables to MIMIC user"
+  psql --host "$CONTAINER_HOSTNAME" --username "$POSTGRES_USER" --dbname mimic -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA MIMIC TO MIMIC;"
+
+  echo "Initialization complete."
+fi

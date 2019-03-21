afb2cedd2a6c44d8b757afd4dfadc3a30a77e25d
tkolleh <tinatuh@gmail.com>
2019-03-21 13:23:34 -0400

    increase numeric precision

--- populate_mimic_db_exclude_chartevents.sh
+++ populate_mimic_db_exclude_chartevents.sh
@@ -5,9 +5,9 @@ echo "Populating mimic-iii database"
 
 export PGOPTIONS="--search_path=MIMIC --client_min_messages=warning"
 
-echo "Loading data into the postgres database from *.csv.gz files at $MIMIC_DATA_DIR"
-cd $MIMIC_DATA_DIR
-psql -f postgres_load_data_gz_exclude_chartevents.sql -v mimic_data_dir="$MIMIC_DATA_DIR"
+echo "Loading data into the postgres database from *.csv.gz files at /var/lib/postgresql/mdata"
+cd /var/lib/postgresql/mdata
+psql --username "$POSTGRES_USER" --dbname mimic -f postgres_load_data_gz_exclude_chartevents.sql -v mimic_data_dir="/var/lib/postgresql/mdata"
 
 echo "Running postgres_add_indexes.sql"
 psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_indexes.sql

7ffef4e5e846f2ea016149dab2d516c1634a0562
tkolleh <tinatuh@gmail.com>
2019-03-21 11:07:50 -0400

    Change column data type

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ COPY postgres_create_tables.sql /docker-entrypoint-initdb.d/psql/
 COPY postgres_add_comments.sql /docker-entrypoint-initdb.d/psql/
 COPY postgres_add_indexes.sql /docker-entrypoint-initdb.d/psql/
 COPY postgres_add_constraints.sql /docker-entrypoint-initdb.d/psql/
+COPY populate_mimic_db.sh /docker-entrypoint-initdb.d/psql/
+COPY populate_mimic_db_exclude_chartevents.sh /docker-entrypoint-initdb.d/psql/
 
 RUN chmod -R a+r /docker-entrypoint-initdb.d/psql/
 
---
+++ populate_mimic_db_exclude_chartevents.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Populating mimic-iii database"
+
+export PGOPTIONS="--search_path=MIMIC --client_min_messages=warning"
+
+echo "Loading data into the postgres database from *.csv.gz files at $MIMIC_DATA_DIR"
+cd $MIMIC_DATA_DIR
+psql -f postgres_load_data_gz_exclude_chartevents.sql -v mimic_data_dir="$MIMIC_DATA_DIR"
+
+echo "Running postgres_add_indexes.sql"
+psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_indexes.sql
+
+echo "Running postgres_add_constraints.sql"
+psql --username "$POSTGRES_USER" --dbname mimic < /docker-entrypoint-initdb.d/psql/postgres_add_constraints.sql
+
+echo "Data load complete"
